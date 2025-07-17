39e06169d32c5fad9c656245a8308975afd16969
Krithik-22 <krithik.for.work@gmail.com>
2025-07-17 18:51:28 +0000

    feat: done with operators and expressions

--- basics.py
+++ basics.py
@@ -11,3 +11,15 @@ userName = input("Enter your name: ")
 userAge = int(input("Enter your age: "))
 #print("Hello ", userName, ", In 5 years you will be ", userAge + 5)
 print(f"Hello {userName}! In 5 years you will be {userAge + 5}")
+
+#Learning Operators & Expressions
+#Mini Challenge
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(f"Sum: {num1 + num2}\nDifference: {num1 - num2}\n")
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num2 > num1:
+    print(f"{num2} is greater than {num1}")
+else:
+    print("Both are equal")
