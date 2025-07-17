b520d351be54e03bf9c3f931b92f437d420ef2d6
Krithik-22 <krithik.for.work@gmail.com>
2025-07-17 18:58:09 +0000

    feat: done with conditionals

--- basics.py
+++ basics.py
@@ -14,7 +14,7 @@ print(f"Hello {userName}! In 5 years you will be {userAge + 5}")
 
 #Learning Operators & Expressions
 #Mini Challenge
-num1 = int(input("Enter a number: "))
+'''num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 print(f"Sum: {num1 + num2}\nDifference: {num1 - num2}\n")
 if num1 > num2:
@@ -23,3 +23,14 @@ elif num2 > num1:
     print(f"{num2} is greater than {num1}")
 else:
     print("Both are equal")
+'''
+#Learning Conditionals
+#Mini Challenge
+haveVoterId = input("Do you have a Voter ID? ")
+
+if userAge >= 18 and haveVoterId == 'yes':
+    print('You are eligible to vote')
+elif userAge >= 18 and haveVoterId == 'no':
+    print('You are eligible to vote but you need a voter ID')
+else:
+    print('You cannot vote')
