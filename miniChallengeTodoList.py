133a8db2b90408b5668a3b16ca1ed8245b24466d
Krithik-22 <krithik.for.work@gmail.com>
2025-07-19 17:52:05 +0000

    fix: added some additional challenges in to do list challenge

--- miniChallengeTodoList.py
+++ miniChallengeTodoList.py
@@ -1,18 +1,46 @@
 tasks = []
-print("Welcome to your To-Do List Manager!\n1. Add Task\n2. View Tasks\n3.Remove Task\n4.Quit")
+print("Welcome to your To-Do List Manager!\n1. Add Task\n2. View Tasks\n3.Remove Task\n4.Mark a task as done\n5. Quit")
 
 while True:
-    choice = int(input("Enter your choice: "))
+    try:
+        choice = int(input("Enter your choice: "))
+    except ValueError:
+        print("Enter a valid number choice")
+        continue
 
     if choice == 1:
         task = input("Enter a task to add: ")
-        tasks.append(task.lower())
-        print("Task Added!")
-    if choice == 2:
-        print(f"Tasks\n{tasks}")
+        if task not in tasks:
+            tasks.append(task.lower())
+            print("Task Added!")
+        else:
+            print("Task already in our To do")
     
-    if choice == 3:
+    elif choice == 2:
+        if not tasks:
+            print("You do not have any tasks yet")
+        else:
+            print(f"Tasks:\n{tasks}")
+    
+    elif choice == 3:
         task = input("Enter a task to remove: ")
-        tasks.remove(task.lower())
-    if choice == 4:
-        break;
+        if task.isdigit():
+            task = int(task)
+            tasks.pop(task-1)
+        else:
+            if task in tasks:
+                tasks.remove(task.lower())
+            else:
+                print("No such task in you tasks list")
+                continue;
+    elif choice == 4:
+        if not tasks:
+            print("No tasks to mark as done")
+        else:
+            task = int(input("Enter a task number to mark as done:")) - 1
+            tasks[task] = f"✔ {tasks[task]}"
+        print(f"Tasks: {tasks}")
+    elif choice == 5:
+        break
+    else:
+        print("Enter a valid choice between 1-4")
