a43c5eb64c76fa59896939412ebd9120182e9405
Krithik-22 <krithik.for.work@gmail.com>
2025-07-28 16:54:30 +0000

    feat: Polymorphism and composition

--- OOP/intermediate.py
+++ OOP/intermediate.py
@@ -5,9 +5,23 @@ class Engine:
     def stop(self):
         print("Engine Stopped...")
 
+class Petrol(Engine):
+    def start(self):
+        print("Vroom! Petrol engine started")
+    
+    def stop(self):
+        print("Brrrrr! Petrol engine stopped")
+
+class Electric(Engine):
+    def start(self):
+        print("ZZZZZ! Electric engine started")
+    
+    def stop(self):
+        print("ZZZZZZ! Electric engine stopped")
+
 class Car:
-    def __init__(self):
-        self.engine = Engine()
+    def __init__(self, engine):
+        self.engine = engine
     
     def start(self):
         self.engine.start()
@@ -16,3 +30,4 @@ class Car:
     def stop(self):
         print("Car has stopped...")
         self.engine.stop()
+
